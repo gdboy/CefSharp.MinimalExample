ed282d72bed359f368969afcd8b68d0bd611f124
llq <664301573@qq.com>
2017-07-16 18:02:23 +0800

    简易浏览器

--- CefSharp.MinimalExample.WinForms/BrowserForm.cs
+++ CefSharp.MinimalExample.WinForms/BrowserForm.cs
@@ -13,133 +13,63 @@ namespace CefSharp.MinimalExample.WinForms
     {
         private readonly ChromiumWebBrowser browser;
 
+        private string homepage = "baidu.com";
+
         public BrowserForm()
         {
+            browser = new ChromiumWebBrowser(homepage)
+            {
+                Dock = DockStyle.Fill,
+            };
+
+            Controls.Add(browser);
+
             InitializeComponent();
 
             Text = "CefSharp";
             WindowState = FormWindowState.Maximized;
 
-            browser = new ChromiumWebBrowser("www.google.com")
-            {
-                Dock = DockStyle.Fill,
-            };
-            toolStripContainer.ContentPanel.Controls.Add(browser);
-
             browser.LoadingStateChanged += OnLoadingStateChanged;
-            browser.ConsoleMessage += OnBrowserConsoleMessage;
             browser.StatusMessage += OnBrowserStatusMessage;
-            browser.TitleChanged += OnBrowserTitleChanged;
-            browser.AddressChanged += OnBrowserAddressChanged;
-
-            var bitness = Environment.Is64BitProcess ? "x64" : "x86";
-            var version = String.Format("Chromium: {0}, CEF: {1}, CefSharp: {2}, Environment: {3}", Cef.ChromiumVersion, Cef.CefVersion, Cef.CefSharpVersion, bitness);
-            DisplayOutput(version);
-        }
-
-        private void OnBrowserConsoleMessage(object sender, ConsoleMessageEventArgs args)
-        {
-            DisplayOutput(string.Format("Line: {0}, Source: {1}, Message: {2}", args.Line, args.Source, args.Message));
-        }
-
-        private void OnBrowserStatusMessage(object sender, StatusMessageEventArgs args)
-        {
-            this.InvokeOnUiThreadIfRequired(() => statusLabel.Text = args.Value);
         }
 
         private void OnLoadingStateChanged(object sender, LoadingStateChangedEventArgs args)
         {
-            SetCanGoBack(args.CanGoBack);
-            SetCanGoForward(args.CanGoForward);
+            this.InvokeOnUiThreadIfRequired(() => backButton.Enabled = args.CanGoBack);
+            this.InvokeOnUiThreadIfRequired(() => forwardButton.Enabled = args.CanGoForward);
 
-            this.InvokeOnUiThreadIfRequired(() => SetIsLoading(!args.CanReload));
+            this.InvokeOnUiThreadIfRequired(() => reloadButton.Visible = args.CanReload);
+            this.InvokeOnUiThreadIfRequired(() => stopButton.Visible = !args.CanReload);
         }
 
-        private void OnBrowserTitleChanged(object sender, TitleChangedEventArgs args)
+        private void OnBrowserStatusMessage(object sender, StatusMessageEventArgs args)
         {
-            this.InvokeOnUiThreadIfRequired(() => Text = args.Title);
+            this.InvokeOnUiThreadIfRequired(() => toolStripStatusLabel.Text = args.Value);
         }
 
-        private void OnBrowserAddressChanged(object sender, AddressChangedEventArgs args)
-        {
-            this.InvokeOnUiThreadIfRequired(() => urlTextBox.Text = args.Address);
-        }
-
-        private void SetCanGoBack(bool canGoBack)
-        {
-            this.InvokeOnUiThreadIfRequired(() => backButton.Enabled = canGoBack);
-        }
-
-        private void SetCanGoForward(bool canGoForward)
-        {
-            this.InvokeOnUiThreadIfRequired(() => forwardButton.Enabled = canGoForward);
-        }
-
-        private void SetIsLoading(bool isLoading)
-        {
-            goButton.Text = isLoading ?
-                "Stop" :
-                "Go";
-            goButton.Image = isLoading ?
-                Properties.Resources.nav_plain_red :
-                Properties.Resources.nav_plain_green;
-
-            HandleToolStripLayout();
-        }
-
-        public void DisplayOutput(string output)
-        {
-            this.InvokeOnUiThreadIfRequired(() => outputLabel.Text = output);
-        }
-
-        private void HandleToolStripLayout(object sender, LayoutEventArgs e)
-        {
-            HandleToolStripLayout();
-        }
-
-        private void HandleToolStripLayout()
-        {
-            var width = toolStrip1.Width;
-            foreach (ToolStripItem item in toolStrip1.Items)
-            {
-                if (item != urlTextBox)
-                {
-                    width -= item.Width - item.Margin.Horizontal;
-                }
-            }
-            urlTextBox.Width = Math.Max(0, width - urlTextBox.Margin.Horizontal - 18);
-        }
-
-        private void ExitMenuItemClick(object sender, EventArgs e)
-        {
-            browser.Dispose();
-            Cef.Shutdown();
-            Close();
-        }
-
-        private void GoButtonClick(object sender, EventArgs e)
-        {
-            LoadUrl(urlTextBox.Text);
-        }
-
-        private void BackButtonClick(object sender, EventArgs e)
+        private void BackButton_Click(object sender, EventArgs e)
         {
             browser.Back();
         }
 
-        private void ForwardButtonClick(object sender, EventArgs e)
+        private void ForwardButton_Click(object sender, EventArgs e)
         {
             browser.Forward();
         }
 
-        private void UrlTextBoxKeyUp(object sender, KeyEventArgs e)
+        private void ReloadButton_Click(object sender, EventArgs e)
         {
-            if (e.KeyCode != Keys.Enter)
-            {
-                return;
-            }
+            browser.Reload();
+        }
 
-            LoadUrl(urlTextBox.Text);
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            browser.Stop();
+        }
+
+        private void HomeButton_Click(object sender, EventArgs e)
+        {
+            LoadUrl(homepage);
         }
 
         private void LoadUrl(string url)
